0836abd11db9f228b733e9135aa290e2aaa78c35
taehwankang97 <kwth2002hs@naver.com>
2024-08-30 17:47:35 +0900

    240830 JDBC용 작성

--- General/Scripts/Script-1.sql
+++ General/Scripts/Script-1.sql
@@ -10,3 +10,79 @@ AND
 SALARY BETWEEN 4000000 AND 8000000
 ORDER BY 
 SALARY ASC;
+
+-- 모든 유저 조회 
+SELECT
+ USER_NO,
+ USER_ID,
+ USER_PW,
+ USER_NAME,
+ TO_CHAR(ENROLL_DATE,'YYYY"년" MM"월" DD "일"') ENROLL_DATE
+FROM TB_USER
+ORDER BY USER_NO ASC;
+
+
+
+-- 검색어가 이름 O 조회
+SELECT
+	USER_NO,
+	USER_ID,
+	USER_PW,
+	USER_NAME,
+	TO_CHAR(ENROLL_DATE,'YYYY"년" MM"월" DD "일"') ENROLL_DATE
+FROM	TB_USER
+WHERE 
+	USER_NAME LIKE '%'||'이'||'%'
+ORDER BY 
+USER_NO ASC;
+
+DELETE 
+	*
+FROM
+	TB_USER
+WHERE
+	USER_NO = 8;
+	-- 일치하는 USER_NO이 있음 1
+ -- 없음 0
+
+
+
+SELECT
+	USER_NO
+FROM
+	TB_USER
+WHERE 
+	USER_ID = 'user02'
+AND 
+	USER_PW = 'user02';	
+	
+-- ID,PW 가 일치하는 이름의 회원 
+
+UPDATE TB_USER
+SET 
+	USER_NAME = '이순신'
+WHERE 
+	USER_NO = 8;
+	
+-- 중복 되는 아이디가 있는지 조회
+SELECT
+COUNT(*)
+FROM
+	TB_USER
+WHERE USER_ID = 'user02';
+
+SELECT
+*
+FROM
+TB_USER;
+
+SELECT
+*
+FROM
+TB_USER
+WHERE
+USER_NO = '4';
+
+
+
+
